0caedf50e1a8a0bc866bfab98a11b46b2d498438
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-06-09 08:56:38 +0200

    Update BFS.h

--- src/traversal/BFS.h
+++ src/traversal/BFS.h
@@ -1,1 +1,19 @@
+#ifndef BFS_H
+#define BFS_H
 
+#include "../graph/Graph.h"
+#include <queue>
+
+class Bfs {
+public:
+    template<typename Discover, typename Finish>
+    static void run(
+        const Graph& g,
+        int start,
+        Discover discover,
+        Finish finish);
+};
+
+#include "BFS.tpp"
+
+#endif

dd4b8d9af752a15ed53205891211fa3281d6cfee
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-06-09 08:52:38 +0200

    Update Graph.h

--- src/Graph/Graph.h
+++ src/Graph/Graph.h
@@ -1,1 +1,35 @@
+#ifndef GRAPH_H
+#define GRAPH_H
 
+#include <vector>
+#include <utility>
+
+class Graph {
+private:
+    int n;
+    std::vector<std::vector<int>> adj;
+    std::vector<int> indegree;
+    std::vector<int> outdegree;
+    std::vector<std::pair<int,int>> edges;
+
+public:
+    Graph();
+
+    bool hasedge(int u,int v) const;
+    void add_edge(int u,int v);
+    void remove_edge(int u,int v);
+
+    void read_edges();
+    void read_mtx();
+    void resize(int n);
+
+    int sources() const;
+    int size() const;
+
+    const std::vector<std::vector<int>>& adjlist() const;
+    const std::vector<int>& indegrees() const;
+    const std::vector<int>& outdegrees() const;
+    const std::vector<std::pair<int,int>>& getedges() const;
+};
+
+#endif

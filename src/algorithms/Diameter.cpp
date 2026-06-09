c75c2093129a1dd2650b13b9e6ba12e2f984387f
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-06-09 09:13:21 +0200

    Update Diameter.cpp

--- src/algorithms/Diameter.cpp
+++ src/algorithms/Diameter.cpp
@@ -1,1 +1,22 @@
+#include "Diameter.h"
+#include "../traversal/BFS.h"
 
+int Diameter::compute(const Graph& g)
+{
+    int diam = 0;
+		for(int s = 0; s < g.size(); s++){
+			vector<int> dist(g.size(),-1);
+			Bfs::run(g,s,
+			[&](int u, int parent, int d){
+				dist[u] = d;
+			},
+			[&](int u){});
+
+			for(int d: dist){
+				if(d == -1) return -1;
+				diam = max(d,diam);
+			}
+
+		}
+		return diam;
+}

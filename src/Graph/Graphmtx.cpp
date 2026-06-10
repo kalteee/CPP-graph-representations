1b6cad215d81bd4f0d36eb7d4e6f816cf4fb7121
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-06-10 10:46:29 +0200

    Update Graphmtx.cpp

--- src/Graph/Graphmtx.cpp
+++ src/Graph/Graphmtx.cpp
@@ -1,16 +1,16 @@
 #include "Graphmtx.h"
 
-GraphMatrix::GraphMatrix() : n(0) {}
+Graphmtx::GraphMatrix() : n(0) {}
 
-void GraphMatrix::resize(int n_) {
+void Graphmtx::resize(int n_) {
     n = n_;
     mat.assign(n, std::vector<int>(n,0));
 }
 
-void GraphMatrix::add_edge(int u,int v){
+void Graphmtx::add_edge(int u,int v){
     mat[u][v] = 1;
 }
 
-bool GraphMatrix::hasedge(int u,int v) const{
+bool Graphmtx::hasedge(int u,int v) const{
     return mat[u][v];
 }

2f9fad32a0fa6f58d99fd2567f6e3bbf1f306b4a
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-08-15 13:15:06 +0200

    Update Graphmtx.cpp

--- src/Graph/Graphmtx.cpp
+++ src/Graph/Graphmtx.cpp
@@ -1,16 +1,56 @@
-#include "Graphmtx.h"
+#include "GraphMatrix.h"
 
-Graphmtx::Graphmtx() : n(0) {}
+GraphMatrix::GraphMatrix() : n(0) {}
 
-void Graphmtx::resize(int n_) {
-    n = n_;
-    mat.assign(n, std::vector<int>(n,0));
+GraphMatrix::GraphMatrix(int n_) {
+    resize(n_);
 }
 
-void Graphmtx::add_edge(int u,int v){
+void GraphMatrix::resize(int n_) {
+    n = n_;
+    mat.assign(n, std::vector<int>(n, 0));
+}
+
+int GraphMatrix::size() const {
+    return n;
+}
+
+bool GraphMatrix::has_edge(int u, int v) const {
+    if (u < 0 || u >= n || v < 0 || v >= n)
+        return false;
+
+    return mat[u][v] != 0;
+}
+
+void GraphMatrix::add_edge(int u, int v) {
+    if (u < 0 || u >= n || v < 0 || v >= n)
+        return;
+
     mat[u][v] = 1;
 }
 
-bool Graphmtx::hasedge(int u,int v) const{
-    return mat[u][v];
+void GraphMatrix::remove_edge(int u, int v) {
+    if (u < 0 || u >= n || v < 0 || v >= n)
+        return;
+
+    mat[u][v] = 0;
+}
+
+std::vector<int> GraphMatrix::neighbors(int u) const {
+    std::vector<int> result;
+
+    if (u < 0 || u >= n)
+        return result;
+
+    for (int v = 0; v < n; ++v) {
+        if (mat[u][v])
+            result.push_back(v);
+    }
+
+    return result;
+}
+
+const std::vector<std::vector<int>>&
+GraphMatrix::matrix() const {
+    return mat;
 }

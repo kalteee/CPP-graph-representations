115f1bd9d709ffb4deb569775d5c9026f8f0dfe1
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-08-15 13:10:27 +0200

    Update IGraph.h

--- include/graph/IGraph.h
+++ include/graph/IGraph.h
@@ -7,8 +7,15 @@ class IGraph {
 public:
     virtual int size() const = 0;
 
-    virtual std::vector<int>
-    neighbors(int u) const = 0;
+    virtual bool has_edge(int u, int v) const = 0;
+
+    virtual void add_edge(int u, int v) = 0;
+
+    virtual void remove_edge(int u, int v) = 0;
+
+    virtual std::vector<int> neighbors(int u) const = 0;
 
     virtual ~IGraph() = default;
 };
+
+#endif

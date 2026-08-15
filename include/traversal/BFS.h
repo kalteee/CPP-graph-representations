d32222b8b6ecffe347622da810f2751c3ab41ed0
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-08-15 13:17:26 +0200

    Update BFS.h

--- include/traversal/BFS.h
+++ include/traversal/BFS.h
@@ -7,25 +7,38 @@
 class Bfs{
 public:
 template<typename Discover, typename Finish>
-static void run(const Graph& g, int start, Discover discover, Finish finish){
-	int n = g.size();
-	vector<int> dist(n,-1);
-	queue<int> q;
-	q.push(start);
-	dist[start] = 0;
-	discover(start,-1,0);
-	const auto &adj = g.adjlist();
-	while(!q.empty()){
-		int u = q.front(); q.pop();
-		for(int v : adj[u]){
-			if(dist[v] == -1){
-				dist[v] = dist[u] + 1;
-				discover(v,u,dist[v]);
-				q.push(v);
-			}
-		}
-		finish(u);
-	}
+static void run(
+    const IGraph& g,
+    int start,
+    Discover discover,
+    Finish finish
+) {
+    int n = g.size();
+
+    vector<int> dist(n, -1);
+    queue<int> q;
+
+    q.push(start);
+    dist[start] = 0;
+
+    discover(start, -1, 0);
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        for (int v : g.neighbors(u)) {
+            if (dist[v] == -1) {
+                dist[v] = dist[u] + 1;
+
+                discover(v, u, dist[v]);
+
+                q.push(v);
+            }
+        }
+
+        finish(u);
+    }
 }
 };
 

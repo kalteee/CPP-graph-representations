4b0876d258c504a6c8739bbf022f2ae713e9360e
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-08-16 19:16:34 +0200

    Update Diameter.cpp

--- src/algorithms/Diameter.cpp
+++ src/algorithms/Diameter.cpp
@@ -1,22 +1,26 @@
-#include "Diameter.h"
-#include "../traversal/BFS.h"
+#include "../../include/algorithms/Diameter.h"
+#include "../../include/traversal/BFS.h"
+#include <vector>
+#include <algorithm>
 
-int Diameter::compute(const Graph& g)
+using namespace std;
+
+int Diameter::compute(const IGraph& g)
 {
     int diam = 0;
-		for(int s = 0; s < g.size(); s++){
-			vector<int> dist(g.size(),-1);
-			Bfs::run(g,s,
-			[&](int u, int parent, int d){
-				dist[u] = d;
-			},
-			[&](int u){});
+    for(int s = 0; s < g.size(); s++){
+        vector<int> dist(g.size(), -1);
+        Bfs::run(g, s,
+            [&](int u, int parent, int d){
+                dist[u] = d;
+            },
+            [&](int u){}
+        );
 
-			for(int d: dist){
-				if(d == -1) return -1;
-				diam = max(d,diam);
-			}
-
-		}
-		return diam;
+        for(int d : dist){
+            if(d == -1) return -1; // Ha a gráf nem összefüggő
+            diam = max(d, diam);
+        }
+    }
+    return diam;
 }

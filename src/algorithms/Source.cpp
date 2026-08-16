0088dd177e80d624077a5a5b4663b89bc993be8a
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-08-16 19:17:05 +0200

    Update Source.cpp

--- src/algorithms/Source.cpp
+++ src/algorithms/Source.cpp
@@ -1,69 +1,92 @@
-#include "../graph/Graph.h"
-#include "../traversal/DFS.h"
-#include "Source.h"
+#include "../../include/algorithms/Source.h"
+#include "../../include/traversal/DFS.h"
+#include "../../include/graph/GraphList.h" // A transzponált gráfhoz kell
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+
 using namespace std;
-static int Source::findsource(const IGraph& g){
-	int n = g.size();
-	vector<int> order;
-	vector<int> state(n,0);
-	for(int s = 0; s<n; s++){
-	if(state[s] != 0) continue;
-		Dfs::run(g,s,state,
-			[&](int u){},
-			[&](int u){order.push_back(u);},
-			[&](int u, int v){});
-	}
 
-	Graph gr;
-	gr.resize(n);
-	for(auto[u,v]:g.getedges()){
-		gr.add_edge(v,u);
-	}
-	vector<int> comp(n,-1);
-	int cid = 0;
-	reverse(order.begin(), order.end());
-	vector<int> state2(n,0);
-	for(int u:order){
-		if(comp[u] != -1) continue;
-		vector<int> st;
-		Dfs::run(gr,u,state2,
-			[&](int v){st.push_back(v);},
-			[&](int v){},
-			[&](int x, int y){});
+int Source::findsource(const IGraph& g){
+    int n = g.size();
+    vector<int> order;
+    vector<int> state(n, 0);
+    
+    // Első DFS: topologikus sorrend előállítása
+    for(int s = 0; s < n; s++){
+        if(state[s] != 0) continue;
+        Dfs::run(g, s, state,
+            [&](int u){},
+            [&](int u){ order.push_back(u); },
+            [&](int u, int v){}
+        );
+    }
 
-		for(int v: st){
-			comp[v] = cid;
-		}
-		cid++;
-	}
+    // Transzponált gráf építése
+    GraphList gr(n);
+    for(int u = 0; u < n; ++u) {
+        for(int v : g.neighbors(u)) {
+            gr.add_edge(v, u);
+        }
+    }
 
-	vector<int> indeg(cid,0);
-	vector<vector<bool>> has_edge(cid, vector<bool>(cid,false));
-	vector<unordered_set<int>> scc_adj(cid);
-	for(auto [u,v]:g.getedges()){
-	int cu = comp[u], cv = comp[v];
-	if(cu != cv) scc_adj[cu].insert(cv);
-	}
-	for(int u = 0; u<cid; u++){
-		for(int v:scc_adj[u]) indeg[v]++;
-	}
-	int src = -1;
-	int cnt = 0;
-	for(int i = 0;i<cid;i++){
-		if(indeg[i] == 0){
-			src = i;
-			cnt++;
-		}
-	}
-	if(cnt != 1){
-	return -1;
-	}else{
-	int best = n;
-	for(int i = 0; i<n; i++){
-		if(comp[i] == src){
-			best = min(best,i);
-		}
-	}
-	return best;
-	}
+    vector<int> comp(n, -1);
+    int cid = 0;
+    reverse(order.begin(), order.end());
+    vector<int> state2(n, 0);
+    
+    // Második DFS a transzponált gráfon (SCC keresés)
+    for(int u : order){
+        if(comp[u] != -1) continue;
+        vector<int> st;
+        Dfs::run(gr, u, state2,
+            [&](int v){ st.push_back(v); },
+            [&](int v){},
+            [&](int x, int y){}
+        );
+
+        for(int v : st){
+            comp[v] = cid;
+        }
+        cid++;
+    }
+
+    // Komponens-gráf (DAG) felépítése
+    vector<int> indeg(cid, 0);
+    vector<unordered_set<int>> scc_adj(cid);
+    
+    for(int u = 0; u < n; ++u) {
+        for(int v : g.neighbors(u)) {
+            int cu = comp[u], cv = comp[v];
+            if(cu != cv) scc_adj[cu].insert(cv);
+        }
+    }
+    
+    for(int u = 0; u < cid; u++){
+        for(int v : scc_adj[u]) indeg[v]++;
+    }
+    
+    int src = -1;
+    int cnt = 0;
+    
+    // Keresünk 0 bemenő fokú SCC-t
+    for(int i = 0; i < cid; i++){
+        if(indeg[i] == 0){
+            src = i;
+            cnt++;
+        }
+    }
+    
+    // Ha több "forrás" SCC van, akkor nincs globális forrás
+    if(cnt != 1){
+        return -1;
+    } else {
+        int best = n;
+        for(int i = 0; i < n; i++){
+            if(comp[i] == src){
+                best = min(best, i);
+            }
+        }
+        return best;
+    }
 }

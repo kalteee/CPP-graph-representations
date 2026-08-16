7f145879b609f304b77e65e11cf7a93b75337d1d
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-08-16 19:08:35 +0200

    Update Diameter.h

--- include/algorithms/Diameter.h
+++ include/algorithms/Diameter.h
@@ -1,11 +1,11 @@
 #ifndef DIAMETER_H
 #define DIAMETER_H
 
-#include "../graph/Graph.h"
+#include "../graph/IGraph.h"
 
 class Diameter {
 public:
-    static int compute(const Graph& g);
+    static int compute(const IGraph& g);
 };
 
 #endif
